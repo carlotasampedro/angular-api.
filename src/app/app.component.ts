6ce4231e36581afdbdc1f0ace5cef29c240804f3
carlotasampedro <148756274+carlotasampedro@users.noreply.github.com>
2025-01-22 10:02:05 +0100

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NgFor, RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  posts: any[] = [];
+  constructor(private servicio:DataService){}//inyectamos el servicioS - inyecion de dependencias
+  ngOnInit(){
+    this.servicio.getPosts().subscribe((data:any[])=>{
+      this.posts = data;
+      console.log(this.posts);  
+    });
+  }
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { PracticaComponent } from './practica/practica.component';
+
+export const routes: Routes = [
+
+{path: 'practica',component: PracticaComponent}
+
+];
